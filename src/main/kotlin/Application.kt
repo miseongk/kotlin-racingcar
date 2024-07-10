5594cc9fe40b4a9944eabe11b48992a1dc4602d0
Miseong Kim <miseongk@naver.com>
2024-07-10 17:41:57 +0900

    refactor: `@JvmStatic` 제거 및 최상위 함수로 변경

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,20 +3,13 @@ import domain.strategy.RandomNumberMoveStrategy
 import view.InputView
 import view.OutputView
 
-class Application {
+fun main(args: Array<String>) {
+    val carNames = InputView.readCarNames()
+    val tryCount = InputView.readTryCount()
 
-    companion object {
+    val racingCarGame = RacingCarGame(carNames, tryCount, RandomNumberMoveStrategy())
+    racingCarGame.play()
 
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val carNames = InputView.readCarNames()
-            val tryCount = InputView.readTryCount()
-
-            val racingCarGame = RacingCarGame(carNames, tryCount, RandomNumberMoveStrategy())
-            racingCarGame.play()
-
-            OutputView.printHistory(racingCarGame.getGameHistory())
-            OutputView.printWinnersName(racingCarGame.getWinners())
-        }
-    }
+    OutputView.printHistory(racingCarGame.getGameHistory())
+    OutputView.printWinnersName(racingCarGame.getWinners())
 }
