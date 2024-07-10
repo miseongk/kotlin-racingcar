a55cd1e797fffc8832f598e99f71297c9dbafeb5
Miseong Kim <miseongk@naver.com>
2024-07-10 19:01:19 +0900

    refactor: 상수 companion object 내에 선언

--- src/main/kotlin/domain/strategy/RandomNumberMoveStrategy.kt
+++ src/main/kotlin/domain/strategy/RandomNumberMoveStrategy.kt
@@ -2,17 +2,19 @@ package domain.strategy
 
 import kotlin.random.Random
 
-private const val MOVE_MIN_CONDITION = 4
-private const val MOVE = 1
-private const val STOP = 0
-
 class RandomNumberMoveStrategy : MoveStrategy {
 
     override fun move(): Int {
         val number = Random.nextInt(10)
         if (number >= MOVE_MIN_CONDITION) {
-            return MOVE;
+            return MOVE
         }
-        return STOP;
+        return STOP
+    }
+
+    companion object {
+        private const val MOVE_MIN_CONDITION = 4
+        private const val MOVE = 1
+        private const val STOP = 0
     }
 }
