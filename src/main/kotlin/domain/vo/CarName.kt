84d8e5a7240ce9ba1aad738660cd88435dab5b2e
Miseong Kim <miseongk@naver.com>
2024-07-10 19:09:42 +0900

    refactor: require 키워드로 ㅍvalidation

--- src/main/kotlin/domain/vo/CarName.kt
+++ src/main/kotlin/domain/vo/CarName.kt
@@ -1,10 +1,10 @@
 package domain.vo
 
 class CarName(val value: String) {
-    
+
     init {
-        if (value.length > MAX_CAR_NAME_LENGTH) {
-            throw IllegalArgumentException("이름의 길이는 ${MAX_CAR_NAME_LENGTH}자 이하만 가능합니다.")
+        require(value.length <= MAX_CAR_NAME_LENGTH) {
+            "이름의 길이는 ${MAX_CAR_NAME_LENGTH}자 이하만 가능합니다."
         }
     }
 
